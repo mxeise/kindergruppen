52847ea8680addad6bf8460776110059c0236e49
Max <maxeise@Maximilians-MacBook-Pro.local>
2017-05-09 22:16:33 +0200

    initial

---
+++ src/assets/js/app.js
@@ -0,0 +1,11 @@
+$(document).foundation();
+
+var prev = 0;
+var $window = $(window);
+var nav = $('.scrollhide-nav');
+
+$window.on('scroll', function(){
+  var scrollTop = $window.scrollTop();
+  nav.toggleClass('hidden', scrollTop > prev);
+  prev = scrollTop;
+});
